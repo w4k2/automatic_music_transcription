1d1c4ab32adc8ac3821e04208cf9553d4a35038b
Michał Leś <michalles3@gmail.com>
2023-03-15 19:19:27 +0100

    corrected experiments script

--- transcription_experiments.sh
+++ transcription_experiments.sh
@@ -3,12 +3,12 @@
 function evaluate_model {
     python evaluate.py with weight_file=$1 dataset=GuitarSet
     python evaluate.py with weight_file=$1 dataset=MAPS
-    python evaluate.py with weight_file=$1 dataset=SynthesizedInstruments dataset_root_dir=data/synthesized_piano_and_guitar
+    python evaluate.py with weight_file=$1 dataset=SynthesizedInstruments
 }
-epoches=100
+epoches=2000
 #BASIC TRANSCRIPTION
-python train.py with train_on=MAPS logdir=results/unet_model_trained_on_MAPS model_type=unet refresh=True epoches=$epoches
-python train.py with train_on=GuitarSet logdir=results/unet_model_trained_on_GuitarSet model_type=unet refresh=True epoches=$epoches
+python train.py with train_on=MAPS logdir=results/unet_model_trained_on_MAPS model_type=unet epoches=$epoches
+python train.py with train_on=GuitarSet logdir=results/unet_model_trained_on_GuitarSet model_type=unet epoches=$epoches
 python train.py with train_on=SynthesizedInstruments logdir=results/unet_model_trained_on_SynthesizedInstruments model_type=unet epoches=$epoches
 
 #EVALUATE TRANSCRIPTION
